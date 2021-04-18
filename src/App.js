3cf599f931833c47446424668c4384cae9440b58
Christopher Grace <chris.grace@designinc.co.uk>
2021-04-18 20:36:31 +0100

    Install Axios to handle data
    # installed axios to best handle to requested data
    # setup request function and test requested data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 import Aux from './hoc/Aux';
 import MainNav from './components/layout/MainNav';
 import Layout from './components/layout/Layout';
 
 function App() {
+
+    useEffect(() => {
+
+        function fetchImages() {
+            return axios({
+                method: 'get',
+                url: 'https://api.flickr.com/services/rest',
+                params: {
+                    method: 'flickr.photos.getRecent',
+                    api_key: '91376182c32d3a23c9f7b86d793c07ae',
+                    extras: 'url_n, owner_name, date_taken, views, description',
+                    format: 'json',
+                    nojsoncallback: 1,
+                },
+            })
+        }
+        
+        const imageData = fetchImages()
+        
+        console.log(imageData)
+
+    }, []);
+
     return (
         <Aux>
             <MainNav />
