b596bf15c3335d57215fd822f00c17c3f3b4a8f3
Christopher Grace <chris.grace@designinc.co.uk>
2021-04-19 07:21:39 +0100

    Display the information I need

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Aux from './hoc/Aux';
 import MainNav from './components/layout/MainNav';
 import Layout from './components/layout/Layout';
+import Card from './components/ui/Card';
 
 function App() {
     const [isLoading, setIsloading]       = useState(true);
@@ -17,13 +18,15 @@ function App() {
             params: {
                 method: 'flickr.photos.getRecent',
                 api_key: '91376182c32d3a23c9f7b86d793c07ae',
-                extras: 'url_n, owner_name, date_taken, views, description',
+                extras: 'url_n, owner_name, date_taken, views, description, tags',
                 format: 'json',
                 nojsoncallback: 1,
             },
         }).then(response => {
             setIsloading(false);
-            setloadedPhotos(response.data.photos);
+            setloadedPhotos(response.data.photos.photo);
+            
+            console.log(response.data.photos.photo);
         });
 
     }, []);
@@ -37,9 +40,29 @@ function App() {
                         Loading
                     </div>
                 ) : (
-                    <div>
-                        Response data
-                    </div>
+                    <ul>
+                        {
+                        loadedPhotos.map(photo => (
+                            <li key={photo.id}>
+                                <Card>
+                                    <div>
+                                        <img src={photo.url_n} alt={photo.title} />
+                                    </div>
+                                    <div>
+                                        <h3>{photo.title}</h3>
+                                        <h4>{photo.owner_name}</h4>
+                                    </div>
+                                    <div>
+                                        {photo.description._content}
+                                    </div>
+                                    <div>
+                                        {photo.tags}
+                                    </div>
+                                </Card>
+                            </li>
+                        ))
+                        }
+                    </ul>
                 )}
             </Layout>
         </Aux>
